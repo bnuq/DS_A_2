47537f7ec368a24e8b03d59f45117f5c53864cb4
bnuq <2astwis2@gmail.com>
2022-05-15 22:25:32 +0900

    to BST

--- Part_2/11_Binary_Search_Tree/Exercises/40 isBST.cpp
+++ Part_2/11_Binary_Search_Tree/Exercises/40 isBST.cpp
@@ -14,6 +14,9 @@ class Node
    }
 };
 
+/* 
+    접근방식은 동일했나봐
+ */
 bool isBSTUtil(Node* node, int min, int max)
 {
     /* an empty tree is BST */
@@ -27,6 +30,13 @@ bool isBSTUtil(Node* node, int min, int max)
      
     /* otherwise check the subtrees recursively,
     tightening the min or max constraint */
+    /* 
+        Top Down 방식으로, 해당 노드를 루트로 가지는 트리가
+        주어진 범위 내의 값을 가지는 지를 조사
+
+        해당 서브트리가 가질 수 있는 값의 범위를 먼저 주는 방식
+        먼저 제한을 걸고, 이 서브트리가 그걸 만족하는 지를 확인    
+     */
     return
         isBSTUtil(node->left, min, node->key) &&
         isBSTUtil(node->right, node->key, max); 
